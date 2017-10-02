5eeb34c5727ccb44529b1dd43ba743d7996f35fe
hynra <hynra@yahoo.com>
2017-10-02 10:37:56 +0700

    login activity - early

--- app/src/main/java/id/pptik/semutangkot/WizardActivity.java
+++ app/src/main/java/id/pptik/semutangkot/WizardActivity.java
@@ -1,6 +1,7 @@
 package id.pptik.semutangkot;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -152,6 +153,7 @@ public class WizardActivity extends AppCompatActivity {
 
 
     private void toLoginActivity(){
+        startActivity(new Intent(mContext, LoginActivity.class));
         finish();
     }
 }
