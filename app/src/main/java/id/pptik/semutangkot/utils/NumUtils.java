0dbff49858c87988262fe272d3cb081a2604fa62
hynra <hynra@yahoo.com>
2017-11-21 15:01:57 +0700

    convert mongo date

--- app/src/main/java/id/pptik/semutangkot/utils/NumUtils.java
+++ app/src/main/java/id/pptik/semutangkot/utils/NumUtils.java
@@ -1,6 +1,14 @@
 package id.pptik.semutangkot.utils;
 
 
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static id.pptik.semutangkot.helper.TimeHelper.getTimeAgo;
+
 public class NumUtils {
 
     public static double round(double value, int places) {
@@ -11,4 +19,32 @@ public class NumUtils {
         long tmp = Math.round(value);
         return (double) tmp / factor;
     }
+
+
+    public static String convertMongoDate(String val){
+        ISO8601DateFormat df = new ISO8601DateFormat();
+        SimpleDateFormat outputFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date d = df.parse(val);
+            String finalStr = outputFormat.format(d);
+            val = finalStr;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return val;
+    }
+
+    public static String convertMongoDateToAgo(String val){
+        ISO8601DateFormat df = new ISO8601DateFormat();
+        SimpleDateFormat outputFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date d = df.parse(val);
+            String finalStr = outputFormat.format(d);
+            val = getTimeAgo(finalStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return val;
+    }
+
 }
